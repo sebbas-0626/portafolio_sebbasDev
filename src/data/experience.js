7ae4666244bc55d4eed303b0ae70b172bedbd181
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-11-19 14:56:53 -0500

    feat: :sparkles: Agregar datos de experiencia laboral y actualizar imagen del proyecto de red social

---
+++ src/data/experience.js
@@ -0,0 +1,19 @@
+export const experience = [
+  {
+    title: "Frontend Developer",
+    period: "septiembre 2023 - octubre 2025",
+    company: "Cootranshuila Ltda",
+    proyecto: "Pagina web cootranshuila ltda",
+    source: "https://cootranshuila.com/",
+    description:
+      "Forme parte del desarrollo de la pagina web actual de la empresa Cootranshuila Ltda, utilizando tecnologias como Next.js, Vite, Tailwind CSS y TypeScript. Implementacion de pasarela de pagos para la compra de tiquetes en linea.",
+  },
+  {
+    title: "Desarrollador Web",
+    period: "Mayo 2023 - Noviembre 2023",
+    company: "Clínica Hispana",
+    proyecto: "pagina web Clínica Hispana",
+    source: "https://www.clinicavirgenguadalupe.com/es/",
+    description:"Desarrollé y completé dos proyectos en WordPress utilizando Divi y Elementor.",
+  }
+];
