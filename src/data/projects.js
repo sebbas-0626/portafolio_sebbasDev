ab98a796f7e8a335e2c570767b2b9efeedab3038
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-11-19 01:04:07 -0500

    feat: :sparkles: Agregar datos de proyectos y habilidades al portafolio

---
+++ src/data/projects.js
@@ -0,0 +1,87 @@
+export const projectsData = [
+  {
+    title: "Proyecto microservicios red social ",
+    description:
+      "clon de threand instagram con arquitectura de microservicios, usando node + sequelize para el backend y angular para el frontend.",
+    image: "",
+    tags: [
+      {
+        icon: "/public/img/skills/angular.svg",
+        name: "Angular",
+      },
+      {
+        icon: "/public/img/skills/Swagger.svg",
+        name: "Swagger",
+      },
+      {
+        icon: "/public/img/skills/node.svg",
+        name: "Node.js",
+      },
+      {
+        icon: "/img/skills/typescript.svg",
+        name: "TypeScript",
+      },
+      {
+        icon: "/public/img/skills/sequelize.svg",
+        name: "Sequelize",
+      },
+    ],
+    source: "https://github.com/sebbas-0626/red-social-prueba",
+  },
+  {
+    title: "Pagina web cootranshuila ltda",
+    description:
+      "Pagina web para venta de pasajes en linea, con pasarela de pagos incluida.",
+    image: "/public/img/project/web_cootranshuila.png",
+    tags: [
+      {
+        icon: "/public/img/skills/nextJS.svg",
+        name: "Nest.js",
+      },
+      {
+        icon: "/img/skills/vitejs.svg",
+        name: "Vite",
+      },
+      {
+        icon: "/public/img/skills/tailwind.svg",
+        name: "Tailwind CSS",
+      },
+      {
+        icon: "/img/skills/typescript.svg",
+        name: "TypeScript",
+      },
+    ],
+    source: "https://github.com/Cootranshuila-LTDA",
+  },
+  {
+    title: "Backend Cootranshuila ltda",
+    description:
+      "Desarrollo para la gestion de rutas, ventas de tiquetes , usuarios, vehiculos y reportes de una empresa de transporte.",
+    image: "/img/project/cootranshuila.png",
+    tags: [
+      {
+        icon: "/img/skills/laravel.svg",
+        name: "Laravel",
+      },
+    ],
+    source: "https://github.com/oscarruiz05/backend-cootranshuila",
+  },
+
+  {
+    title: "Sistema de Inventario para Panaderia",
+    description:
+      "Sistema de Invenatrio para una panaderia, permite gestionar productos, categorias, proveedores y ventas.",
+    image: "/public/img/project/sitema_inventario.png",
+    tags: [
+      {
+        icon: "/img/skills/vue.svg",
+        name: "Vue.js",
+      },
+      {
+        icon: "/img/skills/laravel.svg",
+        name: "Laravel",
+      },
+    ],
+    source: "https://github.com/oscarruiz05/inventario",
+  },
+];
---
+++ src/data/skills.js
@@ -0,0 +1,135 @@
+export const skillsData = [
+
+  {name: 'Node JS', icon: '/public/img/skills/node.svg'},
+  {name: 'Sequelize', icon: '/img/skills/sequelize.svg'},
+  {name: 'Angular', icon: '/img/skills/angular.svg'},
+  {name: 'Linux', icon: '/img/skills/linux.svg'},
+  // {name: 'Nest JS', icon: '/img/skills/nestjs.svg'},
+  {name: 'Next JS', icon: '/img/skills/nextjs.svg'},
+  {name: 'GitHub', icon: '/img/skills/github.svg'},
+  {name: 'Vue', icon: '/img/skills/vue.svg'},
+  {name: 'HTML', icon: '/img/skills/html.svg'},
+  {name: 'CSS', icon: '/img/skills/css.svg'},
+  {name: 'Javascript', icon: '/img/skills/javascript.svg'},
+  {name: 'Typescript', icon: '/img/skills/typescript.svg'},
+  {name: 'React', icon: '/img/skills/react.svg'},
+  {name: 'Tailwind', icon: '/img/skills/tailwindcss.svg'},
+  {name: 'MongoDB', icon: '/img/skills/mongodb.svg'},
+  {name: 'MySQL', icon: '/img/skills/mysql.svg'},
+  {name: 'PostgreSQL', icon: '/img/skills/postgresql.svg'},
+  {name: 'Git', icon: '/img/skills/git.svg'},
+  {name: 'AWS', icon: '/img/skills/aws.svg'},
+  {name: 'Bootstrap', icon: '/img/skills/bootstrap.svg'},
+  {name: 'Docker', icon: '/img/skills/docker.svg'},
+  {name: 'Figma', icon: '/img/skills/figma.svg'},
+  // {name: 'Firebase', icon: '/img/skills/firebase.svg'},
+  {name: 'MaterialUI', icon: '/img/skills/materialui.svg'},
+  {name: 'Nginx', icon: '/img/skills/nginx.svg'},
+  // {name: 'Strapi', icon: '/img/skills/strapi.svg'},
+  {name: 'ViteJS', icon: '/img/skills/vitejs.svg'},
+  {name: 'VuetifyJS', icon: '/img/skills/vuetifyjs.svg'},
+
+]
+
+// AVAILABLE SKILLS
+
+/*
+  HTML
+  CSS
+  JS
+  React
+  Next JS
+  Nuxt JS
+  Node JS
+  Vue
+  Angular
+  Docker
+  Photoshop
+  Illustrator
+  Svelte
+  GCP
+  Azure
+  Fastify
+  Haxe
+  Ionic
+  Markdown
+  Microsoft Office
+  Picsart
+  Sketch
+  Unity
+  WolframAlpha
+  Adobe XD
+  After Effects
+  Bootstrap
+  Bulma
+  CapacitorJs
+  Coffeescript
+  MemSQL
+  C
+  C++
+  C#
+  Python
+  Java
+  Julia
+  Matlab
+  Swift
+  Ruby
+  Kotlin
+  Go
+  PHP
+  Flutter
+  Dart
+  Typescript
+  Swift
+  Git
+  Figma
+  Canva
+  Ubuntu
+  Bootstrap
+  MongoDB
+  Tailwind
+  ViteJS
+  VuetifyJS
+  MySQL
+  PostgreSQL
+  Prisma
+  AWS
+  Firebase
+  Blender
+  Premiere Pro
+  Adobe Audition
+  Deno
+  Django
+  Gimp
+  Graphql
+  Lightroom
+  MaterialUI
+  Nginx
+  Numpy
+  OpenCV
+  Pytorch
+  Selenium
+  Strapi
+  Tensorflow
+  Webex
+  Wordpress
+
+  'Javascript',
+  'Typescript',
+  'React',
+  'Next JS',
+  'Tailwind',
+  'MongoDB',
+  'MySQL',
+  'PostgreSQL',
+  'Git',
+  'AWS',
+  'Bootstrap',
+  'Docker',
+  'Go',
+  'Figma',
+  'Firebase',
+  'MaterialUI',
+  'Nginx',
+  'Strapi',
+*/
